f680c6a1c6846e9a41b597fe128414d5049a6b6b
0954510 <45757323+0954510@users.noreply.github.com>
2019-02-05 09:58:08 +0100

    Update GITHUBTEST.py
    Cijfers

--- GITHUBTEST.py
+++ GITHUBTEST.py
@@ -1,2 +1,21 @@
-4**4
-4*4
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+KeyboardInterrupt
+>>> 
+===== RESTART: C:/Users/lamar/Documents/GitHub/Index.html/GITHUBTEST.py =====
+>>> 
+===== RESTART: C:/Users/lamar/Documents/GitHub/Index.html/GITHUBTEST.py =====
+>>> 
+KeyboardInterrupt
+>>> def u
+SyntaxError: invalid syntax
+>>> 
+>>> 4*10
+40
+>>> 
+>>> 5*5
+25
+>>> 
+===== RESTART: C:/Users/lamar/Documents/GitHub/Index.html/GITHUBTEST.py =====
+>>> 
